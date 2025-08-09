ce561944ff7e3a246b69e9367832ad723c7b6737
Ilham Fadhil <ilham.sykes@hotmail.com>
2025-08-09 22:44:10 +0700

    Add Analytics by Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Navigation } from '@/components/Navigation';
 import { Footer } from '@/components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -34,6 +35,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Navigation />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
